40d24461f381fdf98cbdf20da6b0173ab7967be6
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2023-03-27 16:54:39 -0500

    Basic variables and constructor
    Will be added onto with JSON integration.

--- ProfileControls.cs
+++ ProfileControls.cs
@@ -3,9 +3,24 @@ using System;
 
 public class UserProfile
 {
-    public UserProfile()
-    {
+    //Base variables. More can be added later.
+    private string username;
+    private string password;
+    private string name;
+    private string region;
 
+    //Encapsulation.
+    public string Username { get => username; set => username = value; }
+    public string Password { get => password; set => password = value; }
+    public string Name { get => name; set => name = value; }
+    public string Region { get => region; set => region = value; }
+
+    public UserProfile(string username, string password, string name, string region)
+    {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.region = region;
     }
 }
 
