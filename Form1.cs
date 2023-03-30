e04393c45a1a7ce1a27cb76f15d3d0a7fed5624c
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2023-03-30 10:08:32 -0500

    Make everything work

--- Form1.cs
+++ Form1.cs
@@ -14,5 +14,14 @@ namespace CIS_424_Final
         private void Form1_Load(object sender, EventArgs e)
         {
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.UserControl.LogOut();
+            MessageBox.Show("You have successfully logged out.");
+            LoginForm loginForm = new();
+            loginForm.Show();
+            this.Hide();
+        }
     }
 }
