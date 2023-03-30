cf005e3467f6759826aceeb96d45299eed791207
Maxwell Morfitt <Max.Morfitt@trojans.dsu.edu>
2023-03-29 22:17:07 -0500

    undone redirect for now, will implement soon

--- IntroForm.cs
+++ IntroForm.cs
@@ -20,18 +20,11 @@ namespace IntroForm
         private void btnLogin_Click(object sender, EventArgs e)
         {
             // send user to the login form
-            LoginForm loginRedirect = new LoginForm();
-            loginRedirect.Show();
-            this.Hide();
         }
             
         private void btnRegister_Click(object sender, EventArgs e)
         {
             // send user to the register form
-            // possibly fix names if the register form is different
-            /*RegisterForm registerRedirect = new RegisterForm();
-            registerRedirect.Show();
-            this.Hide();*/
         }
     }
 }
