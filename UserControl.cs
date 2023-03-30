fe4c6600af61b698687232ca9dd145360e74eb63
John Dandy <john.dandy@comcast.net>
2023-03-29 20:13:45 -0500

    Add functionality for having a user logged in and logging them out

---
+++ UserControl.cs
@@ -0,0 +1,23 @@
+﻿namespace CIS_424_Final
+{
+    public class UserControl
+    {
+        #region Attributes
+        public UserProfile? currentUser { get; set; }
+        #endregion
+
+        #region Constructors
+        public UserControl()
+        {
+            this.currentUser = null;
+        }
+        #endregion
+
+        #region Methods
+        public void LogOut()
+        {
+            this.currentUser = null;
+        }
+        #endregion
+    }
+}
