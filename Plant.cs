910d9538b7f165be59d917c7bdfac418ca1ad209
John Dandy <john.dandy@comcast.net>
2023-04-12 16:57:44 -0500

    Add functionality for storing and retrieving plant information

---
+++ Plant.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace CIS_424_Final
+{
+    public class Plant
+    {
+        #region Attributes
+        public string Name { get; set; }
+        public string Sunlight { get; set; }
+        public string Region { get; set; }
+        #endregion
+
+        #region Constructors
+        [JsonConstructor]
+        public Plant(string name, string sunlight, string region)
+        {
+            this.Name = name;
+            this.Sunlight = sunlight;
+            this.Region = region;
+        }
+        #endregion
+    }
+}
---
+++ PlantControl.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace CIS_424_Final
+{
+    public static class PlantControl
+    {
+        #region Methods
+        public static void AddPlant(string path, Plant plant)
+        {
+            string text = File.ReadAllText(path);
+            text = text.Remove(text.Length - 3, 1);
+            text += ",\n" + JsonConvert.SerializeObject(plant, Formatting.Indented) + "\n}";
+            File.WriteAllText(path, text);
+        }
+
+        public static void DeletePlant(string path, Plant plant)
+        {
+            string text = File.ReadAllText(path);
+            text = text.Replace(",\n" + JsonConvert.SerializeObject(plant, Formatting.Indented), "");
+            File.WriteAllText(path, text);
+        }
+
+        public static Plant RetrievePlant(string path, string name)
+        {
+            string[] plantData = File.ReadAllText(path).Split('{', '}');
+            string plant = Array.Find(plantData, p => p.Contains("\"Name\": \"" + name + "\","));
+            return JsonConvert.DeserializeObject<Plant>("{" + plant + "}");
+        }
+
+        public static void UpdatePlant(string path, Plant plant)
+        {
+            Plant oldPlant = RetrievePlant(path, plant.Name);
+            string text = File.ReadAllText(path);
+            text = text.Replace(JsonConvert.SerializeObject(oldPlant, Formatting.Indented), JsonConvert.SerializeObject(plant, Formatting.Indented));
+            File.WriteAllText(path, text);
+        }
+        #endregion
+
+    }
+}
