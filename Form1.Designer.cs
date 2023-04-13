86c595aeff63ff2261d9aabb49f5489b899423f4
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2023-04-12 23:13:08 -0500

    Completed plant table display
    Max's work

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,95 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             menuStrip1 = new MenuStrip();
             menuToolStripMenuItem = new ToolStripMenuItem();
-            introPageToolStripMenuItem = new ToolStripMenuItem();
+            changeProfileToolStripMenuItem = new ToolStripMenuItem();
             registerationToolStripMenuItem = new ToolStripMenuItem();
             logoutToolStripMenuItem = new ToolStripMenuItem();
-            plantsToolStripMenuItem = new ToolStripMenuItem();
-            regionsToolStripMenuItem = new ToolStripMenuItem();
-            statesToolStripMenuItem = new ToolStripMenuItem();
-            southDakotaToolStripMenuItem = new ToolStripMenuItem();
+            dataGridView1 = new DataGridView();
+            nameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            sunlightDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            regionDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            plantBindingSource = new BindingSource(components);
             menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)plantBindingSource).BeginInit();
             SuspendLayout();
             // 
             // menuStrip1
             // 
             menuStrip1.ImageScalingSize = new Size(32, 32);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem, plantsToolStripMenuItem, regionsToolStripMenuItem });
+            menuStrip1.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(1486, 40);
+            menuStrip1.Padding = new Padding(3, 1, 0, 1);
+            menuStrip1.Size = new Size(800, 24);
             menuStrip1.TabIndex = 1;
             menuStrip1.Text = "menuStrip1";
             // 
             // menuToolStripMenuItem
             // 
-            menuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { introPageToolStripMenuItem, registerationToolStripMenuItem, logoutToolStripMenuItem });
+            menuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { changeProfileToolStripMenuItem, registerationToolStripMenuItem, logoutToolStripMenuItem });
             menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            menuToolStripMenuItem.Size = new Size(97, 36);
+            menuToolStripMenuItem.Size = new Size(50, 22);
             menuToolStripMenuItem.Text = "Menu";
             // 
-            // introPageToolStripMenuItem
+            // changeProfileToolStripMenuItem
             // 
-            introPageToolStripMenuItem.Name = "introPageToolStripMenuItem";
-            introPageToolStripMenuItem.Size = new Size(359, 44);
-            introPageToolStripMenuItem.Text = "IntroPage";
-            introPageToolStripMenuItem.Click += introPageToolStripMenuItem_Click;
+            changeProfileToolStripMenuItem.Name = "changeProfileToolStripMenuItem";
+            changeProfileToolStripMenuItem.Size = new Size(152, 22);
+            changeProfileToolStripMenuItem.Text = "Change Profile";
+            changeProfileToolStripMenuItem.Click += changeProfileToolStripMenuItem_Click;
             // 
             // registerationToolStripMenuItem
             // 
             registerationToolStripMenuItem.Name = "registerationToolStripMenuItem";
-            registerationToolStripMenuItem.Size = new Size(359, 44);
-            registerationToolStripMenuItem.Text = "Registeration";
+            registerationToolStripMenuItem.Size = new Size(152, 22);
+            registerationToolStripMenuItem.Text = "Registration";
             registerationToolStripMenuItem.Click += registerationToolStripMenuItem_Click;
             // 
             // logoutToolStripMenuItem
             // 
             logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
-            logoutToolStripMenuItem.Size = new Size(359, 44);
+            logoutToolStripMenuItem.Size = new Size(152, 22);
             logoutToolStripMenuItem.Text = "Logout";
             logoutToolStripMenuItem.Click += logoutToolStripMenuItem_Click;
             // 
-            // plantsToolStripMenuItem
+            // dataGridView1
             // 
-            plantsToolStripMenuItem.Name = "plantsToolStripMenuItem";
-            plantsToolStripMenuItem.Size = new Size(97, 36);
-            plantsToolStripMenuItem.Text = "Plants";
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { nameDataGridViewTextBoxColumn, sunlightDataGridViewTextBoxColumn, regionDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = plantBindingSource;
+            dataGridView1.Location = new Point(12, 27);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.ShowEditingIcon = false;
+            dataGridView1.Size = new Size(762, 394);
+            dataGridView1.TabIndex = 2;
             // 
-            // regionsToolStripMenuItem
+            // nameDataGridViewTextBoxColumn
             // 
-            regionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { statesToolStripMenuItem });
-            regionsToolStripMenuItem.Name = "regionsToolStripMenuItem";
-            regionsToolStripMenuItem.Size = new Size(118, 36);
-            regionsToolStripMenuItem.Text = "Regions";
+            nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            nameDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // statesToolStripMenuItem
+            // sunlightDataGridViewTextBoxColumn
             // 
-            statesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { southDakotaToolStripMenuItem });
-            statesToolStripMenuItem.Name = "statesToolStripMenuItem";
-            statesToolStripMenuItem.Size = new Size(210, 44);
-            statesToolStripMenuItem.Text = "States";
+            sunlightDataGridViewTextBoxColumn.DataPropertyName = "Sunlight";
+            sunlightDataGridViewTextBoxColumn.HeaderText = "Sunlight";
+            sunlightDataGridViewTextBoxColumn.Name = "sunlightDataGridViewTextBoxColumn";
+            sunlightDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // southDakotaToolStripMenuItem
+            // regionDataGridViewTextBoxColumn
             // 
-            southDakotaToolStripMenuItem.Name = "southDakotaToolStripMenuItem";
-            southDakotaToolStripMenuItem.Size = new Size(292, 44);
-            southDakotaToolStripMenuItem.Text = "South Dakota";
+            regionDataGridViewTextBoxColumn.DataPropertyName = "Region";
+            regionDataGridViewTextBoxColumn.HeaderText = "Region";
+            regionDataGridViewTextBoxColumn.Name = "regionDataGridViewTextBoxColumn";
+            regionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // plantBindingSource
+            // 
+            plantBindingSource.DataSource = typeof(Plant);
             // 
             // mainPage
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1486, 960);
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
-            Margin = new Padding(6);
             Name = "mainPage";
             Text = "mainPage";
             Load += Form1_Load;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)plantBindingSource).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -131,5 +156,11 @@
         private ToolStripMenuItem regionsToolStripMenuItem;
         private ToolStripMenuItem statesToolStripMenuItem;
         private ToolStripMenuItem southDakotaToolStripMenuItem;
+        private DataGridView dataGridView1;
+        private ToolStripMenuItem changeProfileToolStripMenuItem;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn sunlightDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn regionDataGridViewTextBoxColumn;
+        private BindingSource plantBindingSource;
     }
 }
